f705c89bea1bdbcb821a46c7a5ea3a7eb724f44e
tenglabs <tengoneotechnologies@gmail.com>
2020-11-22 14:40:34 +0200

    wtf

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 from sqlhelpers import *
+from forms import *
+
+
 app =  Flask(__name__)
 
 
@@ -14,10 +17,34 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    users = Table("users", "name", "email", "username", "password")
+
+    if request.method == 'POST' and form.validate():
+        # collect form data
+        username = form.username.data
+        email = form.email.data
+        name = form.name.data
+
+        # make sure user does not already exist
+        if isnewuser(username):
+            # add the user to mysql and log them in
+            password = sha256_crypt.encrypt(form.password.data)
+            users.insert(name, email, username, password)
+            log_in_user(username)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('User already exists', 'danger')
+            return redirect(url_for('register'))
+
+    return render_template('register.html', form=form)
+
+
 @app.route("/")
 def index():
-    users = Table('users', 'name', 'email', 'username', 'password')
-    users.insert('John Doe', 'jd@jd.com', 'johndoe', 'hash')
+
     return render_template('index.html')
 
 
